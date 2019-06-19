956adef0cb37718ae196a22d4148106b6ceca20b
Sachin Mahajan <sachinm78@gmail.com>
2019-06-19 18:08:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 // your code here
 
+const rootURL = 'https://api.github.com';
 
+function getRepositories() {
+  const name = document.getElementById('username').value;
+  const uri = rootURL + '/users/' + name + '/repos';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayRepositories);
+  xhr.open('GET', uri);
+  xhr.send();
+  return false;
+}
